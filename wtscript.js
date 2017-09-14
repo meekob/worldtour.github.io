aab4473417a526f867938fb1fd3fe7dbb1590c85
meekob <meekob@gmail.com>
2017-09-14 14:07:17 +0700

    move map to specific location

--- wtscript.js
+++ wtscript.js
@@ -1,5 +1,6 @@
 
 var map;
+var geocoder;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -7,8 +8,46 @@ function initMap() {
         center: {lat: 40.712784, lng: -74.005941},
         zoom: 12
     });
+
+    geocoder = new google.maps.Geocoder();
 }
+
+function getcodeAddress(geocoder, map) {
+    var address = document.getElementById('search-text').value;
+    console.log(address);
+
+    if (address.length == 0) { return false; }
+
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            console.log('Geocode was not successful for the following reason: ' + status);
+        }
+    });    
+}
+
+document.getElementById('find-button').addEventListener('click', function() {
+    console.log('find clicked.');
+    getcodeAddress(geocoder, map);
+});  
+
+document.querySelector('#search-text').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+        console.log('enter hit.');
+        getcodeAddress(geocoder, map);
+    }
+});
 
 function setFocus() {
     document.getElementById("search-text").focus();
 }
+
+
+document.onload = setFocus();
+
