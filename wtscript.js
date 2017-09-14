bf53396b606d5108f62f6f9bdea636d2033256f5
meekob <meekob@gmail.com>
2017-09-14 14:12:29 +0700

    remove marker

--- wtscript.js
+++ wtscript.js
@@ -21,10 +21,10 @@ function getcodeAddress(geocoder, map) {
     geocoder.geocode({'address': address}, function(results, status) {
         if (status === 'OK') {
             map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
+            // var marker = new google.maps.Marker({
+            //     map: map,
+            //     position: results[0].geometry.location
+            // });
         } else {
             console.log('Geocode was not successful for the following reason: ' + status);
         }
